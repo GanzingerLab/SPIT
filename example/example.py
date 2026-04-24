0d668d57848b2350fd965722c48078c04f37dc11
gcastrolinares <Gerry.7914@gmail.com>
2026-04-24 14:08:25 +0200

    update example to include the new settings for cropping during image acquisition

--- example/example.py
+++ example/example.py
@@ -10,11 +10,21 @@ from spit.SPIT import SPIT_Run, SPIT_Dataset, localize_tiff_run, localize_tiff_d
 
 class RegistrationSettings:
     def __init__(self):
-        self.registration_folder = r'C:\Users\castrolinares\Data analysis\SPIT_G\Raquel_6Feb2024\regis' #Folder containing the H-matrices and crop coordinates needed for the alignment of the channels
+        self.registration_folder = r'D:\Data\Tom\260423_beads_for_Gerard\260423_K2\regis' # r'C:\Users\castrolinares\Data analysis\SPIT_G\Raquel_6Feb2024\regis' #Folder containing the H-matrices and crop coordinates needed for the alignment of the channels
         self.verticalROI = [0, 1100] #Specify the heigth of the channels that you want to use. I am pretty sure that if we set it larger, it still does it correctly. 
         self.to_keep = [0, None] #number of frames to procees. 
         #[0, None] means all frames, if you want to change it, set the specific number ([0:200] would be the first 200 frames 
         #while [300:None] would be from frame 301 until the end)
+        self.width_crop = {
+            1: 672, 
+            2: 1344, 
+            3: 2048
+        } 
+        self.original_crop_annapurna = {
+            'ch1': 0, 
+            'ch2': 680, 
+            'ch3': 1352,
+        }
         
         self.ch_width_annapurna = 640 #The width of the channels in pixels. 
         self.x_coords_annapurna = { # Define start and end X coordinate for each channel. 
@@ -24,14 +34,28 @@ class RegistrationSettings:
             '405nm': (1370, 1370 + self.ch_width_annapurna),
         }
         
-        self.ch_width_K2 = 560 #The width of the channels in pixels. 
-        self.x_coords_K2 = { # Define start and end X coordinate for each channel.
-            '638nm': (60, 60 + self.ch_width_K2),
-            '561nm': (740, 740 + self.ch_width_K2),
-            '488nm': (1415, 1415 + self.ch_width_K2),
-            '405nm': (1415, 1415 + self.ch_width_K2),
+        self.original_crop_K2 = {
+            'ch1': 6, 
+            'ch2': 685, 
+            'ch3': 1365,
         }
-       
+        
+       #K2
+        self.ch_width_K2 = 638   
+        self.x_coords_K2 = { #K2
+            '638nm': (21, 21 + self.ch_width_K2),  
+            '561nm': (701, 701 + self.ch_width_K2),  
+            '488nm': (1375, 1375 + self.ch_width_K2),
+            '405nm': (1375, 1375 + self.ch_width_K2),
+        }
+        #Old K2 values, before alignment by Sebastian
+        # self.ch_width_K2 = 560 #The width of the channels in pixels. 
+        # self.x_coords_K2 = { # Define start and end X coordinate for each channel.
+        #     '638nm': (60, 60 + self.ch_width_K2),
+        #     '561nm': (740, 740 + self.ch_width_K2),
+        #     '488nm': (1415, 1415 + self.ch_width_K2),
+        #     '405nm': (1415, 1415 + self.ch_width_K2),
+        # }
         #####USED ONLY FOR AFFINE TRANSFORM OF TIFF FILES#####
         self.channels = ['m_ch', 'l_ch'] # list with: l_ch = left, m_ch = middle, r_ch=right
         self.microscope = 'K2'  #or ANNAPURNA
@@ -42,14 +66,22 @@ class RegistrationSettings:
             'm_ch': (691, 691 + self.ch_width_annapurna),  
             'r_ch': (1370, 1370 + self.ch_width_annapurna)
         }  
-        
+
         #K2
-        self.ch_width_K2_tiff = 560   
+        self.ch_width_K2_tiff = 638   
         self.x_coords_K2_tiff = { #K2
-            'l_ch': (60, 60 + self.ch_width_K2),  
-            'm_ch': (740, 740 + self.ch_width_K2),  
-            'r_ch': (1415, 1415 + self.ch_width_K2)
+            'l_ch': (21, 21 + self.ch_width_K2),  
+            'm_ch': (701, 701 + self.ch_width_K2),  
+            'r_ch': (1375, 1375 + self.ch_width_K2)
         }
+
+        #K2 values before alignment by Sebastian
+        # self.ch_width_K2_tiff = 560   
+        # self.x_coords_K2_tiff = { #K2
+        #     'l_ch': (60, 60 + self.ch_width_K2),  
+        #     'm_ch': (740, 740 + self.ch_width_K2),  
+        #     'r_ch': (1415, 1415 + self.ch_width_K2)
+        # }
         ######################################################
     
 class LocalizationSettings:
@@ -75,7 +107,7 @@ class LocalizationSettings:
             'qe': 0.9  #In Picasso qe (quantum efficiency) is not used anymore. It is left for bacward compatibility. 
         }
         
-        self.skip = '561'
+        self.skip = ''
         self.suffix = '' #sufix for the name of the file, if necessary.
         self.transform = False #Do non-affine corrections of the localized spots if you have multiple channels.
         self.plot = True 
@@ -85,14 +117,14 @@ class LinkSettings:
         self.coloc = False #Bool --> are you using a colocalization file or not? 
         self.dt = None  #specify the exposure time (in seconds!) or None. If None, the script will look for it in the _result.txt file. 
         self.quick = False #use the quick version? 200px squared from the center and only 500 frames. 
-        self.roi = True #Do you want to filter per ROI? 
+        self.roi = False #Do you want to filter per ROI? 
         self.suffix = '' #sufix for the name of the file, if necessary. 
-        self.fil_len = 20 #filter the length of the tracks. Tracks shorter than this ammount of frames will be filtered
+        self.fil_len = 3 #filter the length of the tracks. Tracks shorter than this ammount of frames will be filtered
         self.fil_diff = 0.0002 #Filter for immobile particles. Tracks with a diffusion coefficient smaller than this number will be filtered
         self.tracker = 'trackpy' #Tracker algorithm to use: trackpy or swift. After talking with Chris, swift is very complicated and the focus of the developers is not 
         # really tracking, but diffusion rates. So, swift is not implemented, and I am not sure if it will. 
-        self.memory = 1 #max number of frames from which a particle can disappear 
-        self.search = 15#max search range for trackpy linking in px 
+        self.memory = 0 #max number of frames from which a particle can disappear 
+        self.search = 5 #max search range for trackpy linking in px 
 
 class ColocTracksSettings:
     def __init__(self):
@@ -118,7 +150,7 @@ class ColocLocsSettings:
         self.suffix = '' #sufix for the name of the file, if necessary. 
         self.dt = None #specify the exposure time (in seconds!) or None. If None, the script will look for it in the _result.txt file. 
         
-settings = settings.Settings(RegistrationSettings, LocalizationSettings, LinkSettings, ColocTracksSettings, ColocLocsSettings)
+settings = Settings(RegistrationSettings, LocalizationSettings, LinkSettings, ColocTracksSettings, ColocLocsSettings)
 #%%
 #In this new version of SPIT, please run SPIT commands under an if __name__ == "__main__": 
                     #It is veri important to avoid problems with multiprocessing (during linking). If you, in the same script start analyzing
@@ -222,7 +254,7 @@ if __name__ == "__main__":
 
 #%%
 if __name__ == "__main__":
-    test4 = SPIT_Run(r'P:\18 REPRESSIT\CELL_experiments\Anna\20260113_AB001_training\pMHC_well1\Run00005', settings)
+    test4 = SPIT_Dataset(r'D:\Data\20251215_T6_mutants', settings)
     # test4.affine_transform()
-    test4.localize()
+    test4.link()
     # test4.roi()
