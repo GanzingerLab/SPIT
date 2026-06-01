df3b9b60bd0cc2c7dbe52a4578ef25efc38cb244
gcastrolinares <Gerry.7914@gmail.com>
2026-06-01 09:08:31 +0200

    link generates CSVs

--- spit/plot_diffusion.py
+++ spit/plot_diffusion.py
@@ -8,7 +8,7 @@ from matplotlib.font_manager import FontProperties
 from spit import tools
 from spit import linking as link
 from spit import localize
-
+import pandas as pd
 
 # %% Localization precision
 def plot_localization_precision(df_locs, path=None, ax=None):
@@ -291,7 +291,23 @@ def plot_msd(df_stats, dt, path=None, plot_loglog=True, ax=None):
     D_msd_median_w = np.average(
         df_stats.D_msd, weights=df_stats.loc_count)  # weighted median
     # D_msd_median = df_tracks.D_msd.quantile(q=0.5)
+    pd.concat([
+    pd.DataFrame({
+        'track_id': i,
+        'lag_time_s': row.lagtimes * dt,
+        'msd_um2': row.msd * nm2um**2
+    })
+    for i, row in enumerate(df_stats.itertuples())
+    ]).to_csv(path + '_msd_raw_data.csv', index=False)
 
+    pd.DataFrame({
+        'lag_time_s': msd_fit_x * dt,
+        'msd_q025_um2': y_fit_q025 * nm2um**2,
+        'msd_q05_um2': y_fit_q05 * nm2um**2,
+        'msd_q075_um2': y_fit_q075 * nm2um**2,
+        'D_msd_weighted_um2_s': D_msd_median_w
+    }).to_csv(path + '_msd_fit_data.csv', index=False)
+    
     ax.text(0.05, 0.95, s=rf'D_MSD = {D_msd_median_w:.2f} $\mu m^2/s$',
             ha='left', va='top', color='k', transform=ax.transAxes,
             bbox=dict(facecolor='ghostwhite', alpha=0.8, edgecolor='lightgrey'))
@@ -358,6 +374,12 @@ def plot_Dmsd(df_stats, dt, path=None, ax=None, color='black', split=False):
             )
 
     D_msd_median_w = np.average(df_stats.D_msd, weights=df_stats.loc_count)
+
+    pd.DataFrame({
+    'D_msd_um2_s': df_stats.D_msd,
+    'weight_loc_count': df_stats.loc_count,
+    'D_msd_weighted_mean_um2_s': D_msd_median_w
+    }).to_csv(path + '_D_msd_data.csv', index=False)
 
     if not split:
         ax.text(0.95, 0.95, s=rf'D_MSD = {D_msd_median_w:.2f} $\mu m^2/s$',
@@ -499,6 +521,22 @@ def plot_jd(df_stats, dt, path=None, ax=None):
         f = tools.pdf_jd_sub(x, dt, pars[2], pars[3])
         exp_freq_sub2.append(f)
 
+    pd.DataFrame({
+    'jump_distance_um': jd_all
+    }).to_csv(path + '_jd_raw_data.csv', index=False)
+
+    pd.DataFrame({
+        'exp_x_um': exp_x,
+        'exp_freq': exp_freq,
+        'exp_freq_immobile': exp_freq_sub1,
+        'exp_freq_mobile': exp_freq_sub2,
+        'D_jd0': pars[0],
+        'A_jd0': pars[1],
+        'D_jd1': pars[2],
+        'A_jd1': pars[3],
+        'dt_s': dt
+    }).to_csv(path + '_jd_fit_data.csv', index=False)
+    
     ax.hist(jd_all,
             bins='fd',
             color='gray',
@@ -631,6 +669,18 @@ def plot_x_per_frame(df, data_type, dt, path=None, roll_param=10, ignore_start=0
     x_fit = np.arange(0, data_per_frame.size*10, 1)
     y_fit = tools.exp_single(x_fit, *popt)
 
+    pd.DataFrame({
+    'frame': data_per_frame.index,
+    'time_s': data_per_frame.index * dt,
+    'normalized_count': data_per_frame.values,
+    'rolling_mean': data_per_frame.rolling(roll_param).mean().values,
+    'fit_value': tools.exp_single(data_per_frame.index, *popt),
+    'fit_param_0': popt[0],
+    'tau_bleach': popt[1],
+    'fit_param_2': popt[2]
+        }).to_csv(path + f'_{data_type}_per_frame_data.csv', index=False)
+
+
     # Plot
     save_plot = False
     if ax is None:
@@ -679,6 +729,12 @@ def plot_NgT(df_tracks, dt, path=None, color='black', split=False, ax=None):
     '''
     Ts, NgT = link.tracks_greaterT(df_tracks, dt)
     Tcrit = Ts[np.argmax(NgT < 0.5)]
+
+    pd.DataFrame({
+    'T_s': Ts,
+    'NgT': NgT,
+    'Tcrit_s': Tcrit
+        }).to_csv(path + '_NgT_data.csv', index=False)
     # max_time = dt*linked.frame.max()
 
     # Plot
@@ -757,6 +813,12 @@ def plot_track_lengths(df_stats, dt, path=None, color='black', split=False, ax=N
                 histtype='step',
                 zorder=100)
         # ax.set_yscale('log')
+    
+    pd.DataFrame({
+    'track_duration_s': df_stats.length * dt,
+    'average_duration_s': average_length * dt,
+    'weighted': weighted
+    }).to_csv(path + 'track_lengths_data.csv', index=False)
 
     if not split:
         ax.text(0.95, 0.95, s=f'Duration = {average_length*dt:.1f} s',
@@ -857,8 +919,7 @@ def plot_track_stats(df_tracks, df_stats, df_statsF, path, px2nm, dt=None):
 
         axs[0, 2].legend([f'roi {x}' for x in nROI], loc='lower right')
 
-    plot_x_per_frame(df_tracksF, 'Tracks', dt, roll_param=10, ignore_start=0,
-                     ax=axs[0, 0])  # should be the filtered ones
+    plot_x_per_frame(df_tracksF, 'Tracks', dt, roll_param=10, ignore_start=0,ax=axs[0, 0])  # should be the filtered ones
     plot_track_lengths(df_statsF, dt, ax=axs[0, 1])
     plot_NgT(df_tracksF, dt, ax=axs[0, 2])
     plot_jd(df_stats, dt, ax=axs[1, 0])
